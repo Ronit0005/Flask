94dc0b3112b5364bacc8578c4992de877852f5d5
Ronti0005 <rontisingh24012007@gmail.com>
2025-08-02 23:16:26 +0530

    Adding the data sciences files to the repo

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route("/")
-def Hello_World():
-    return "<pre>Hii I am Ronit Kumar Singh</pre>"
+from flask import Flask,render_template,request
 
-@app.route("/route")
-def route():
-    return "<p>Hii I am Ronit "
+app=Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 app.run(debug=True)
