0ad3b9d066856f02d18d4d2e4c32385cca25a2d4
Ronti0005 <rontisingh24012007@gmail.com>
2025-08-05 23:04:11 +0530

    Adding the nre file to the repo

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, flash
 
 app=Flask(__name__)
+app.secret_key='10011'
 
 @app.route("/")
 def main():
+    flash("Thanking You !!!","success")
     return render_template("index.html")
+
+@app.route("/sec",methods=["GET","POST"])
+def second():
+    flash("OK byee...")
+    return render_template("index2.html")
+
 
 app.run(debug=True)
